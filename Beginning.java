e466b2452c0d4a3a582127d4b5faa282ca667dad
petalbloom <ga3menon@gmail.com>
2025-03-29 20:22:47 -0400

    Got background to work using swing and graphics/awt classes

--- Beginning.java
+++ Beginning.java
@@ -5,6 +5,22 @@ import java.awt.event.ActionListener;
 
 public class Beginning {
     public static void main(String[] args) {
-        // FIll out
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("Beginning");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(400, 400);
+            frame.add(new DrawingPanel());
+            frame.setVisible(true);
+        });
+    }
+}
+
+class DrawingPanel extends JPanel {
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        // Set background color
+        setBackground(new Color(124, 44, 3, 255));
     }
 }
---
+++ out/production/hhcyoa/README.md
@@ -0,0 +1,4 @@
+# HHCYOA - HooHacks Create Your Own Adventure
+By Charvi Konduru and Gayathri Menon
+
+Welcome to HooHacks Create Your Own Adventure! This is a simple video game where you will be playing as a student participating in the HooHacks hackathon. You can choose how you want to spend your time in the hackathon, and watch the adventure unfold!
